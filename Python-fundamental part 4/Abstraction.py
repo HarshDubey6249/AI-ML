413c796a39ac88bf4f2fb82319bef8dbb203be8e
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-06-28 13:23:33 +0530

    mplement abstract class with ABC and abstractmethod

--- Python-fundamental part 4/Abstraction.py
+++ Python-fundamental part 4/Abstraction.py
@@ -16,3 +16,43 @@ class Cow(Animal):
 lion =Lion();
 
 lion.make_sound()
+
+#-----------------
+# Import ABC (Abstract Base Class) and abstractmethod decorator
+from abc import ABC, abstractmethod
+
+
+# Abstract Class
+# This class cannot be instantiated because it contains an abstract method.
+class Animal(ABC):
+
+    # Abstract Method
+    # Every child class MUST implement this method.
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+
+# Child Class 1
+# Inherits from Animal
+class Lion(Animal):
+
+    # Implements the abstract method
+    def make_sound(self):
+        print("roar")
+
+
+# Child Class 2
+# Inherits from Animal
+class Cow(Animal):
+
+    # Implements the abstract method
+    def make_sound(self):
+        print("wooo")
+
+
+# Creating an object of Lion
+lion = Lion()
+
+# Calling Lion's implementation of make_sound()
+lion.make_sound()
