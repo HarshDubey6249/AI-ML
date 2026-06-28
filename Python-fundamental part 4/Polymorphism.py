f96fb32f52213b6d29369f7012365bedfd672c09
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-06-28 13:50:21 +0530

    function oveiding

---
+++ Python-fundamental part 4/Polymorphism.py
@@ -0,0 +1,12 @@
+class Employee:
+    def get_des(self):
+        print("hi how are you");
+        
+class Teacher:
+    def get_des(self):
+        print("today we go to learn");
+        
+t1=Teacher()
+t1.get_des();
+        
+        
